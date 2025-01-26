a7b4190320e120bad8867fa24b1e77cf048079df
Hari <harijv0310@gmail.com>
2025-01-26 12:38:52 +0530

    Started working, Added header module

---
+++ Frontend/src/Index/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Pdf from './Components/pdf.jsx'
+import './main.css'
+import Header from './Components/Header.jsx'
+
+createRoot(document.getElementById('dashboardbodywrapper')).render(
+  <StrictMode>
+    <Header/>
+    <Pdf />
+  </StrictMode>,
+)
