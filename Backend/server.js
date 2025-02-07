d14158a5374946ddf28a445d9eda8708d7816e56
Hari <harijv0310@gmail.com>
2025-02-07 06:14:12 +0530

    Started working on custom div in resume, separated css

--- Backend/server.js
+++ Backend/server.js
@@ -14,67 +14,64 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 5000;
 
-// Enable CORS
 app.use(cors());
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// Function to compile Handlebars template
 async function compileTemplate(templatePath, data) {
    const templateHtml = fs.readFileSync(templatePath, 'utf8');
    const template = Handlebars.compile(templateHtml);
    return template(data);
 }
 
-// Route to generate PDF
 app.post('/generate-pdf', async (req, res) => {
    const resumeData = req.body;
 
-   // Compile the template with Handlebars
-   const bodyPath = path.join(__dirname, '/Resume Components/body.html');
-   let pdfBody = await compileTemplate(bodyPath, resumeData);
+   let templatePath = path.join(__dirname, '/Resume Components/body.html');
+   let templateFile = fs.readFileSync(templatePath, 'utf8');
+
+   const templateCss = path.join(__dirname, '/Resume Components/body.css');
+   const cssFile = fs.readFileSync(templateCss, 'utf8');
+
+   templateFile = templateFile.replace('<style></style>', `<style>${cssFile}</style>`);
+
+   let compiledTemplate = Handlebars.compile(templateFile);
+   let pdfBody = compiledTemplate(resumeData);
 
    const htmlBodyOutputPath = path.join(__dirname, '/Output/Body.html');
    fs.writeFileSync(htmlBodyOutputPath, pdfBody);
 
    const headerPath = path.join(__dirname, '/Resume Components/header.html');
    const footerPath = path.join(__dirname, '/Resume Components/footer.html');
-   const pdfHeader = fs.readFileSync(headerPath, 'utf8');
-   let pdfFooter = fs.readFileSync(footerPath, 'utf8');
+   const headerFile = fs.readFileSync(headerPath, 'utf8');
+   let footerFile = fs.readFileSync(footerPath, 'utf8');
 
    const auLogoBase64Path = path.join(__dirname, '/Images/Base64 Values/Au Logo.txt');
-   const auLogoBase64 = fs.readFileSync(auLogoBase64Path, 'utf8');
+   const auLogoFile = fs.readFileSync(auLogoBase64Path, 'utf8');
 
+   footerFile = footerFile.replace('<img id="aulogo" src=""', `<img id="aulogo" src="data:image/png;base64,${auLogoFile}"`);
+
+   
    let date_time = new Date();
 
-   // get current date
    let date = ("0" + date_time.getDate()).slice(-2);
 
-   // get current month
    let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
 
-   // get current year
    let year = date_time.getFullYear();
 
-   // get current hours
    let hours = String(date_time.getHours()).padStart(2, '0');
 
-   // get current minutes
    let minutes = String(date_time.getMinutes()).padStart(2, '0');
 
-   // get current seconds
    let seconds = String(date_time.getSeconds()).padStart(2, '0');
 
    const currentTime = `${date}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 
-
-   pdfFooter = pdfFooter.replace('<img id="aulogo" src=""', `<img id="aulogo" src="data:image/png;base64,${auLogoBase64}"`);
-
-   pdfFooter = pdfFooter.replace('<p>Developed by the Department of IST. Generated at</p>', `<p>Developed by the Department of IST. Generated at ${currentTime}</p>`);
+   footerFile = footerFile.replace('<p>Developed by the Department of IST. Generated at</p>', `<p>Developed by the Department of IST. Generated at ${currentTime}</p>`);
 
    const htmlFooterOutputPath = path.join(__dirname, '/Output/Footer.html');
-   fs.writeFileSync(htmlFooterOutputPath, pdfFooter);
+   fs.writeFileSync(htmlFooterOutputPath, footerFile);
 
    const browser = await puppeteer.launch({
       headless: true
@@ -89,14 +86,14 @@ app.post('/generate-pdf', async (req, res) => {
       format: 'A4',
       printBackground: true,
       margin: {
-         top: '15mm', // Extra space for the header
-         bottom: '20mm', // Extra space for the footer
+         top: '15mm', 
+         bottom: '20mm', 
          left: '15mm',
          right: '15mm',
       },
       displayHeaderFooter: true,
-      headerTemplate: pdfHeader,
-      footerTemplate: pdfFooter,
+      headerTemplate: headerFile,
+      footerTemplate: footerFile,
    });
 
    await page.screenshot({
--- Frontend/src/Index/Components/pdf.jsx
+++ Frontend/src/Index/Components/pdf.jsx
@@ -3,125 +3,126 @@ import axios from 'axios';
 
 const GeneratePDF = () => {
     const [resumeData, setResumeData] = useState({
-        username: "Hari Prasad V",
-        small_bio: "Developer|Designer|Freelancer",
-        phone_number: "+91 9090909099",
-        emailid: "hari@gmail.com",
-        location: "Chennai, India",
-        linkedin: "www.linkedin.com/Hari",
-        linkedinlink: "https://www.linkedin.com/Hari",
-        github: "www.github.com/Hari",
-        githublink: "https://www.github.com/Hari",
-        summary:"I am a full stack developer with 3 years of experience in web development. I have worked on multiple projects and have a good understanding of web technologies.I am a full stack developer with 3 years of experience in web development. I have worked on multiple projects and have a good understanding of web technologies.",
-        education: [
-            {
-                phd: 
-                [
-                    {
-                        phd_name: "Ph.D in Network Security",
-                        phd_university: "Anna University",
-                        phd_year: "2024 - 2029",
-                        phd_exp: "Expertise : Networking, Security, Machine Learning, Hash Algorithms",
-                        phd_additional_info: "Widely accepted research"
-                    }
-                ],
-                pg_degree: 
-                [
-                    {
-                        pg_degree_name: "Master of Computer Applications",
-                        pg_degree_university: "Anna University",
-                        pg_degree_year: "2022 - 2024",
-                        pg_degree_cgpa: "CGPA : 9.5",
-                        pg_additional_info: "As of Semester 3"
-                    }
-                ],
-                ug_degree: 
-                [
-                    {
-                        ug_degree_name: "B.Sc. Computer Science",
-                        ug_degree_university: "Anna University",
-                        ug_degree_year: "2019 - 2022",
-                        ug_degree_cgpa: "CGPA : 8.7",
-                        ug_additional_info: "Distinction Grade"
-                    }
-                ],
-                diploma: 
-                [
-                    {
-                        diploma_name: "Diploma in Technology",
-                        diploma_university: "Anna University",
-                        diploma_year: "2017 - 2019",
-                        diploma_cgpa: "CGPA : 9.5",
-                        diploma_additional_info: "Distinction Grade"
-                    }
-                ],
-                hsc_name:"Jawahar School", 
-                hsc_year:"2015 - 2017", 
-                hsc_grade: "Grade : 68.0",
-                hsc_additional_info: "State Board",
-                sslc_name:"Jawahar School", 
-                sslc_year:"2014 - 2015", 
-                sslc_grade: "Grade : 68.0",
-                sslc_additional_info: "State Board"
-            }
-        ],
-        skills: 
-        [
-            {
-                skillstyle1: true, 
-                skillset: ["Web Development", "Cloud Deployment", "Machine Learning"]
-            },
-            /*{
-                skillstyle2: true, 
-                skillset: "Web Development, Cloud Deployment, Machine Learning"
-            }*/
-        ],
-        projects:
-        [
-            {
-                project_name:"Project 1", 
-                project_link:"https://github.com/Hari/Project1",
-                project_description:"Developed and maintained the company website", 
-                project_tech: "HTML, CSS, JavaScript, ReactJS, NodeJS, MongoDB"
-            },
-            {
-                project_name:"Project 2", 
-                project_link:"https://github.com/Hari/Project2",
-                project_description:"Developed and maintained the company website", 
-                project_tech: "HTML, CSS, JavaScript, ReactJS, NodeJS, MongoDB"
-            }
-        ],
-        experience: [   
-            {
-                experience_company:"ABC Corp,",
-                experience_location:"Bangalore", 
-                experience_year:"2022 - Present", 
-                experience_designation:"Software Developer", 
-                experience_team:"Cloud Deployment", 
-                experience_roles: [
-                    "Developed and maintained the company website","Developed and maintained the company website"
-                    ],
-            },
-            /*{
-                experience_company:"ABCD Corp,",
-                experience_location:"Hyderabad", 
-                experience_year:"2023 - Present", 
-                experience_designation:"Software Developer", 
-                experience_description: "I am a full stack developer with 3 years of experience in web development. I have worked on multiple projects and have a good understanding of web technologies.I am a full stack developer with 3 years of experience in web development. I have worked on multiple projects and have a good understanding of web technologies."
-            },*/
-        ],
-        certifications: 
-        [
-            {
-                certificationstyle1: true, 
-                certificationset: ["AWS Certified Developer (www.awsverification.com/ghvsgv212)", "Google Cloud Certified", "Machine Learning Expert"]
-            },
-            /*{
-                certificationstyle2: true, 
-                certificationset: "AWS Certified Developer, Google Cloud Certified (udemy.com/certificate/ghvsgv212), Machine Learning Expert"
-            }*/
-        ],
-        languages: ["English (Full professional proficiency)", "Tamil (Mother Tongue)", "Hindi (Limited working proficiency)"],
+        username: "John Doe",
+  small_bio: "Developer|Designer|Freelancer",
+  phone_number: "+91 9090909099",
+  emailid: "john@gmail.com",
+  location: "Chennai, India",
+  linkedin: "linkedin.com/John",
+  linkedinlink: "https://www.linkedin.com/John",
+  github: "github.com/John",
+  githublink: "https://www.github.com/John",
+  summary: "I am a full stack developer with 3 years of experience in web development. I have worked on multiple projects and have a good understanding of web technologies. I specialize in building scalable web applications using modern frameworks and cloud technologies.",
+  education: [
+    {
+      phd: [
+        {
+          phd_name: "Ph.D in Network Security",
+          phd_university: "Princeton University",
+          phd_year: "2024 - 2029",
+          phd_exp: "Expertise: Networking, Security, Machine Learning, Hash Algorithms",
+          phd_additional_info: "Widely accepted research in cybersecurity protocols and machine learning"
+        }
+      ],
+      pg_degree: [
+        {
+          pg_degree_name: "Master of Computer Applications",
+          pg_degree_university: "Princeton University",
+          pg_degree_year: "2022 - 2024",
+          pg_degree_cgpa: "CGPA: 9.5",
+          pg_additional_info: "As of Semester 3"
+        }
+      ],
+      ug_degree: [
+        {
+          ug_degree_name: "B.Sc. Computer Science",
+          ug_degree_university: "Princeton University",
+          ug_degree_year: "2019 - 2022",
+          ug_degree_cgpa: "CGPA: 8.7",
+          ug_additional_info: "Distinction Grade"
+        }
+      ],
+      diploma: [
+        {
+          diploma_name: "Diploma in Technology",
+          diploma_university: "Princeton University",
+          diploma_year: "2017 - 2019",
+          diploma_cgpa: "CGPA: 9.5",
+          diploma_additional_info: "Distinction Grade"
+        }
+      ],
+      hsc_name: "Don Bosco School",
+      hsc_year: "2015 - 2017",
+      hsc_grade: "Grade: 68.0",
+      hsc_additional_info: "State Board",
+      sslc_name: "Don Bosco School",
+      sslc_year: "2014 - 2015",
+      sslc_grade: "Grade: 68.0",
+      sslc_additional_info: "State Board"
+    }
+  ],
+  skills: [
+    {
+      skillstyle1: true,
+      skillset: ["Web Development", "Cloud Deployment", "Machine Learning"]
+    }
+  ],
+  projects: [
+    {
+      project_name: "Project 1",
+      project_link: "https://github.com/John/Project1",
+      project_description: "Developed and maintained the company website with advanced features like authentication, user profiles, and data management.",
+      project_tech: "HTML, CSS, JavaScript, ReactJS, NodeJS, MongoDB"
+    },
+    {
+      project_name: "Project 2",
+      project_link: "https://github.com/John/Project2",
+      project_description: "Built a responsive and interactive e-commerce platform with integrated payment systems and a product recommendation engine.",
+      project_tech: "HTML, CSS, JavaScript, ReactJS, NodeJS, MongoDB"
+    }
+  ],
+  experience: [
+    {
+      experience_company: "Amazon India",
+      experience_location: "Bangalore",
+      experience_year: "2022 - Present",
+      experience_designation: "Software Developer",
+      experience_team: "Cloud Deployment",
+      experience_roles: [
+        "Developed and maintained the company website using modern JavaScript frameworks",
+        "Collaborated with cross-functional teams to deploy cloud solutions and improve scalability"
+      ]
+    }
+  ],
+  certifications: [
+    {
+      certificationstyle1: true,
+      certificationset: [
+        "AWS Certified Developer (www.awsverification.com/ghvsgv212)",
+        "Google Cloud Certified",
+        "Machine Learning Expert"
+      ]
+    }
+  ],
+  languages: [
+    "English (Full professional proficiency)",
+    "Tamil (Mother Tongue)",
+    "Hindi (Limited working proficiency)"
+  ],
+  customdiv:
+  [
+    {
+      customtitle: "Custom Title 1",
+      customdivstyle1: true,
+      customlist: ["Custom Point 1", "Custom Point 2", "Custom Point 3"]
+    },
+    {
+      customtitle: "Custom Title 2",
+      customdivstyle2: true,
+      customparagraph: "Custom Point 1 Custom Point 2 Custom Point 3"
+    },
+
+  ]
     });
 
     const handleGeneratePDF = async () => {
