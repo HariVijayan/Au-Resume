ded14323092e11f01c319179721bc5d2c06fd82f
Hari <harijv0310@gmail.com>
2025-02-20 19:07:34 +0530

    Refactored Backend server code

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,7 @@ import puppeteer from 'puppeteer';
 import fs from 'fs';
 import path from 'path';
 import Handlebars from 'handlebars';
-import {
-   fileURLToPath
-} from 'url';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,132 +12,116 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 5000;
 
-Handlebars.registerHelper("or", function () {
-   return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
-});
+Handlebars.registerHelper("or", (...args) => args.some(Boolean));
 
 app.use(cors());
-
 app.use(express.json());
+
+const readFileSync = (filePath) => fs.readFileSync(path.join(__dirname, filePath), 'utf8');
+const writeFileSync = (filePath, data) => fs.writeFileSync(path.join(__dirname, filePath), data);
 
 function removeEmptyValues(obj) {
    if (Array.isArray(obj)) {
        const filteredArray = obj
-           .map(removeEmptyValues) // Recursively clean array items
-           .filter(item => item !== null); // Remove null entries
+           .map(removeEmptyValues)
+           .filter(item => item !== null); 
        return filteredArray.length > 0 ? filteredArray : null;
    } else if (typeof obj === "object" && obj !== null) {
        let newObj = {};
        for (let key in obj) {
            const cleanedValue = removeEmptyValues(obj[key]);
-
-           // Only add non-null values to the object
            if (cleanedValue !== null) {
                newObj[key] = cleanedValue;
            }
        }
-
-       // If the object only has boolean flags (like customdivstyle1), remove it
        const hasOnlyBooleans = Object.values(newObj).every(val => typeof val === "boolean");
 
        return Object.keys(newObj).length > 0 && !hasOnlyBooleans ? newObj : null;
    } else {
-       return obj === "" ? null : obj; // Remove empty strings
+       return obj === "" ? null : obj;
    }
 }
 
-app.post('/generate-pdf', async (req, res) => {
-   const resumeData = removeEmptyValues(req.body);
+const getCurrentTimestamp = () => {
+  const date_time = new Date();
+  const date = String(date_time.getDate()).padStart(2, '0');
+  const month = String(date_time.getMonth() + 1).padStart(2, '0');
+  const year = date_time.getFullYear();
+  const hours = String(date_time.getHours()).padStart(2, '0');
+  const minutes = String(date_time.getMinutes()).padStart(2, '0');
+  const seconds = String(date_time.getSeconds()).padStart(2, '0');
+  return `${date}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+};
 
-   //console.log(resumeData);
+const generateFooter = (footerFile, timestamp) => {
+  return footerFile.replace('<p>Developed by the Department of IST. Generated at</p>', 
+                            `<p>Developed by the Department of IST. Generated at ${timestamp}</p>`);
+};
 
-   let templatePath = path.join(__dirname, '/Resume Components/body.html');
-   let templateFile = fs.readFileSync(templatePath, 'utf8');
-
-   const templateCss = path.join(__dirname, '/Resume Components/body.css');
-   const cssFile = fs.readFileSync(templateCss, 'utf8');
-
-   templateFile = templateFile.replace('<style></style>', `<style>${cssFile}</style>`);
-
-   let compiledTemplate = Handlebars.compile(templateFile);
-   let pdfBody = compiledTemplate(resumeData);
-
-   const htmlBodyOutputPath = path.join(__dirname, '/Output/Body.html');
-   fs.writeFileSync(htmlBodyOutputPath, pdfBody);
-
-   const resumeDataOutput = path.join(__dirname, '/Output/Resume Data.txt');
-   const resumeDataString = JSON.stringify(resumeData, null, 2);
-   fs.writeFileSync(resumeDataOutput, resumeDataString);
-
-   const headerPath = path.join(__dirname, '/Resume Components/header.html');
-   const footerPath = path.join(__dirname, '/Resume Components/footer.html');
-   const headerFile = fs.readFileSync(headerPath, 'utf8');
-   let footerFile = fs.readFileSync(footerPath, 'utf8');
-
-   const auLogoBase64Path = path.join(__dirname, '/Images/Base64 Values/Au Logo.txt');
-   const auLogoFile = fs.readFileSync(auLogoBase64Path, 'utf8');
-
-   footerFile = footerFile.replace('<img id="aulogo" src=""', `<img id="aulogo" src="data:image/png;base64,${auLogoFile}"`);
-
-   let date_time = new Date();
-
-   let date = ("0" + date_time.getDate()).slice(-2);
-
-   let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
-
-   let year = date_time.getFullYear();
-
-   let hours = String(date_time.getHours()).padStart(2, '0');
-
-   let minutes = String(date_time.getMinutes()).padStart(2, '0');
-
-   let seconds = String(date_time.getSeconds()).padStart(2, '0');
-
-   const currentTime = `${date}-${month}-${year} ${hours}:${minutes}:${seconds}`;
-
-   footerFile = footerFile.replace('<p>Developed by the Department of IST. Generated at</p>', `<p>Developed by the Department of IST. Generated at ${currentTime}</p>`);
-
-   const htmlFooterOutputPath = path.join(__dirname, '/Output/Footer.html');
-   fs.writeFileSync(htmlFooterOutputPath, footerFile);
-
-   const browser = await puppeteer.launch({
-      headless: true
-   });
-   const page = await browser.newPage();
-   await page.setContent(pdfBody, {
-      waitUntil: 'load'
-   });
-
-   const pdfBuffer = await page.pdf({
-      path: 'Output/Resume Output.pdf',
-      format: 'A4',
-      printBackground: true,
-      margin: {
-         top: '15mm', 
-         bottom: '20mm', 
-         left: '15mm',
-         right: '15mm',
-      },
-      displayHeaderFooter: true,
-      headerTemplate: headerFile,
-      footerTemplate: footerFile,
-   });
-
-   await page.screenshot({
-      path: 'Output/ScreenShot.png',
-      fullPage: true
-   })
-
-
-   await browser.close();
-
-   res.type('application/pdf');
-   res.end(pdfBuffer, 'binary');
+const generateHeaderFooterPaths = () => ({
+  headerPath: '/Resume Components/header.html',
+  footerPath: '/Resume Components/footer.html',
+  auLogoBase64Path: '/Images/Base64 Values/Au Logo.txt',
 });
 
+const compileTemplate = (templateFile, resumeData) => {
+  const compiledTemplate = Handlebars.compile(templateFile);
+  return compiledTemplate(resumeData);
+};
+
+const generatePdf = async (htmlContent, headerFile, footerFile) => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setContent(htmlContent, { waitUntil: 'load' });
+
+  const pdfBuffer = await page.pdf({
+    path: 'Output/Resume Output.pdf',
+    format: 'A4',
+    printBackground: true,
+    margin: { top: '15mm', bottom: '20mm', left: '15mm', right: '15mm' },
+    displayHeaderFooter: true,
+    headerTemplate: headerFile,
+    footerTemplate: footerFile,
+  });
+
+  await page.screenshot({ path: 'Output/ScreenShot.png', fullPage: true });
+  await browser.close();
+  return pdfBuffer;
+};
+
+app.post('/generate-pdf', async (req, res) => {
+  const resumeData = removeEmptyValues(req.body);
+
+  // Template and CSS paths
+  const templatePath = '/Resume Components/body.html';
+  const templateCssPath = '/Resume Components/body.css';
+  const headerFooterPaths = generateHeaderFooterPaths();
+
+  const templateFile = readFileSync(templatePath);
+  const cssFile = readFileSync(templateCssPath);
+  const headerFile = readFileSync(headerFooterPaths.headerPath);
+  let footerFile = readFileSync(headerFooterPaths.footerPath);
+  const auLogoFile = readFileSync(headerFooterPaths.auLogoBase64Path);
+
+  // Replace CSS and AU logo in the footer
+  let compiledTemplate = compileTemplate(templateFile.replace('<style></style>', `<style>${cssFile}</style>`), resumeData);
+  footerFile = footerFile.replace('<img id="aulogo" src=""', `<img id="aulogo" src="data:image/png;base64,${auLogoFile}"`);
+
+  const timestamp = getCurrentTimestamp();
+  footerFile = generateFooter(footerFile, timestamp);
+
+  // Write Output Files
+  writeFileSync('/Output/Body.html', compiledTemplate);
+  writeFileSync('/Output/Resume Data.txt', JSON.stringify(resumeData, null, 2));
+  writeFileSync('/Output/Footer.html', footerFile);
+
+  // Generate PDF
+  const pdfBuffer = await generatePdf(compiledTemplate, headerFile, footerFile);
+
+  res.type('application/pdf');
+  res.end(pdfBuffer, 'binary');
+});
 
 app.listen(port, () => {
-   console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
-
-
