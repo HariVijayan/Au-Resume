2dedc375312f544c69c315768a038b5f99deb049
Hari <harijv0310@gmail.com>
2025-04-06 05:11:30 +0530

    Added basic implementation of info div to dynamic Experience RB Page

--- Frontend/src/Components/Resume_Builder/Sub_Components/Experience/ExperienceMain.jsx
+++ Frontend/src/Components/Resume_Builder/Sub_Components/Experience/ExperienceMain.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-import Style1 from './Style 1';
-import Style2 from './Style 2';
-import PreviewPdf from '../PreviewPdf.jsx';
-import { useNavigate } from 'react-router-dom';
+import Style1 from "./Style 1";
+import Style2 from "./Style 2";
+import PreviewPdf from "../PreviewPdf.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Experience = ({ resumeData, setResumeData, templateType }) => {
   const navigate = useNavigate();
 
   const changeContent = (navigationType) => {
-    if(navigationType==="previous"){
-      navigate('/resume-builder/bio-summary');
-    }else{
-      navigate('/resume-builder/education/phd');
+    if (navigationType === "previous") {
+      navigate("/resume-builder/bio-summary");
+    } else {
+      navigate("/resume-builder/education/phd");
     }
   };
 
@@ -82,46 +82,103 @@ const Experience = ({ resumeData, setResumeData, templateType }) => {
   return (
     <div id="dv-MainFormAndPreview">
       <div id="dv-MainForm">
-    <div id="dv-ExperienceWrapper" className="WrapperClass">
-      <div id="dv-ExperienceStyle1Header" className="ExperienceHeader">
-        <h3>Experience <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M160-200v-440 440-15 15Zm0 80q-33 0-56.5-23.5T80-200v-440q0-33 23.5-56.5T160-720h160v-80q0-33 23.5-56.5T400-880h160q33 0 56.5 23.5T640-800v80h160q33 0 56.5 23.5T880-640v171q-18-13-38-22.5T800-508v-132H160v440h283q3 21 9 41t15 39H160Zm240-600h160v-80H400v80ZM720-40q-83 0-141.5-58.5T520-240q0-83 58.5-141.5T720-440q83 0 141.5 58.5T920-240q0 83-58.5 141.5T720-40Zm20-208v-112h-40v128l86 86 28-28-74-74Z"/></svg></h3>
-        <button
-          type="button"
-          onClick={() => setExperience("Style1")}
-          className="ListInputButton"
-        >
-         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M360-200v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360ZM200-160q-33 0-56.5-23.5T120-240q0-33 23.5-56.5T200-320q33 0 56.5 23.5T280-240q0 33-23.5 56.5T200-160Zm0-240q-33 0-56.5-23.5T120-480q0-33 23.5-56.5T200-560q33 0 56.5 23.5T280-480q0 33-23.5 56.5T200-400Zm0-240q-33 0-56.5-23.5T120-720q0-33 23.5-56.5T200-800q33 0 56.5 23.5T280-720q0 33-23.5 56.5T200-640Z"/></svg> List Type
-        </button>
+        <div id="dv-ExperienceWrapper" className="WrapperClass">
+          <div id="dv-ExperienceStyle1Header" className="ExperienceHeader">
+            <h3>
+              Experience{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#e3e3e3"
+              >
+                <path d="M160-200v-440 440-15 15Zm0 80q-33 0-56.5-23.5T80-200v-440q0-33 23.5-56.5T160-720h160v-80q0-33 23.5-56.5T400-880h160q33 0 56.5 23.5T640-800v80h160q33 0 56.5 23.5T880-640v171q-18-13-38-22.5T800-508v-132H160v440h283q3 21 9 41t15 39H160Zm240-600h160v-80H400v80ZM720-40q-83 0-141.5-58.5T520-240q0-83 58.5-141.5T720-440q83 0 141.5 58.5T920-240q0 83-58.5 141.5T720-40Zm20-208v-112h-40v128l86 86 28-28-74-74Z" />
+              </svg>
+            </h3>
+            <button
+              type="button"
+              onClick={() => setExperience("Style1")}
+              className="ListInputButton"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#e3e3e3"
+              >
+                <path d="M360-200v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360ZM200-160q-33 0-56.5-23.5T120-240q0-33 23.5-56.5T200-320q33 0 56.5 23.5T280-240q0 33-23.5 56.5T200-160Zm0-240q-33 0-56.5-23.5T120-480q0-33 23.5-56.5T200-560q33 0 56.5 23.5T280-480q0 33-23.5 56.5T200-400Zm0-240q-33 0-56.5-23.5T120-720q0-33 23.5-56.5T200-800q33 0 56.5 23.5T280-720q0 33-23.5 56.5T200-640Z" />
+              </svg>{" "}
+              List Type
+            </button>
 
-        <button
-          type="button"
-          onClick={() => setExperience("Style2")}
-          className="ParaInputButton"
-        >
-         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h240v80H120Zm0-200v-80h480v80H120Zm0-200v-80h720v80H120Z"/></svg> Para Type
-        </button>
+            <button
+              type="button"
+              onClick={() => setExperience("Style2")}
+              className="ParaInputButton"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#e3e3e3"
+              >
+                <path d="M120-240v-80h240v80H120Zm0-200v-80h480v80H120Zm0-200v-80h720v80H120Z" />
+              </svg>{" "}
+              Para Type
+            </button>
+          </div>
+
+          {experienceType === "Default" && (
+            <p>Please select an experience type to begin.</p>
+          )}
+
+          {renderedStyles.includes("Style1") && (
+            <Style1 resumeData={resumeData} setResumeData={setResumeData} />
+          )}
+
+          {renderedStyles.includes("Style2") && (
+            <Style2 resumeData={resumeData} setResumeData={setResumeData} />
+          )}
+        </div>
+        <div id="dv-ExperienceButtons" className="NavigationButtons">
+          <button
+            type="button"
+            onClick={() => changeContent("previous")}
+            className="LeftNavigationButtons"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z" />
+            </svg>{" "}
+            Basic Details
+          </button>
+          <button
+            type="button"
+            onClick={() => changeContent("next")}
+            className="RightNavigationButtons"
+          >
+            Ph.D{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z" />
+            </svg>
+          </button>
+        </div>
       </div>
-
-      {experienceType === "Default" && <p>Please select an experience type to begin.</p>}
-
-      {renderedStyles.includes("Style1") && (
-        <Style1 resumeData={resumeData} setResumeData={setResumeData} />
-      )}
-
-      {renderedStyles.includes("Style2") && (
-        <Style2 resumeData={resumeData} setResumeData={setResumeData} />
-      )}
-    </div>
-    <div id="dv-ExperienceButtons" className="NavigationButtons">
-            <button type="button" onClick={() => changeContent("previous")} className="LeftNavigationButtons">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z"/></svg> Basic Details
-            </button>
-            <button type="button" onClick={() => changeContent("next")} className="RightNavigationButtons">
-              Ph.D <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z"/></svg>
-            </button>
-          </div>
-          </div>
-          <PreviewPdf resumeData={resumeData} templateType={templateType}/>
+      <PreviewPdf resumeData={resumeData} templateType={templateType} />
     </div>
   );
 };
--- Frontend/src/Components/Resume_Builder/Sub_Components/Experience/Style 1.jsx
+++ Frontend/src/Components/Resume_Builder/Sub_Components/Experience/Style 1.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Style1 = ({ resumeData, setResumeData }) => {
+  const [infoDiv, setInfoDiv] = useState("");
+
+  const showOrHideInfoDiv = (currentState) => {
+    if (infoDiv === currentState) {
+      setInfoDiv(" ");
+    } else {
+      setInfoDiv(currentState);
+    }
+  };
+
   const handleInputChange = (e, index) => {
     const { name, value } = e.target;
     const updatedExperience = [...resumeData.experience];
@@ -35,7 +45,26 @@ const Style1 = ({ resumeData, setResumeData }) => {
             <label htmlFor="in-rb_exp_name" className="TextFieldLabel">
               Company Name
             </label>
+            <svg
+              onClick={() => showOrHideInfoDiv(`Company Name${index}`)}
+              className="InputInfoSvg"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
           </div>
+
+          {infoDiv === `Company Name${index}` && (
+            <div className="InputInfoDiv">
+              <div className="InputInfoText">
+                <p>Enter your company name</p>
+              </div>
+            </div>
+          )}
           <div id="dv-ExperienceLocation" className="InputWrapper">
             <input
               type="text"
@@ -48,7 +77,26 @@ const Style1 = ({ resumeData, setResumeData }) => {
             <label htmlFor="in-rb_exp_location" className="TextFieldLabel">
               Location
             </label>
+            <svg
+              onClick={() => showOrHideInfoDiv(`Work Location${index}`)}
+              className="InputInfoSvg"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
           </div>
+
+          {infoDiv === `Work Location${index}` && (
+            <div className="InputInfoDiv">
+              <div className="InputInfoText">
+                <p>Enter your work location</p>
+              </div>
+            </div>
+          )}
           <div id="dv-ExperiencePeriod" className="InputWrapper">
             <input
               type="text"
@@ -61,7 +109,26 @@ const Style1 = ({ resumeData, setResumeData }) => {
             <label htmlFor="in-rb_exp_year" className="TextFieldLabel">
               Period
             </label>
+            <svg
+              onClick={() => showOrHideInfoDiv(`Work Period${index}`)}
+              className="InputInfoSvg"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
           </div>
+
+          {infoDiv === `Work Period${index}` && (
+            <div className="InputInfoDiv">
+              <div className="InputInfoText">
+                <p>Enter the period of this work experience</p>
+              </div>
+            </div>
+          )}
           <div id="dv-ExperienceDesignation" className="InputWrapper">
             <input
               type="text"
@@ -74,7 +141,26 @@ const Style1 = ({ resumeData, setResumeData }) => {
             <label htmlFor="in-rb_exp_desg" className="TextFieldLabel">
               Designation
             </label>
+            <svg
+              onClick={() => showOrHideInfoDiv(`Designation${index}`)}
+              className="InputInfoSvg"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
           </div>
+
+          {infoDiv === `Designation${index}` && (
+            <div className="InputInfoDiv">
+              <div className="InputInfoText">
+                <p>Enter the Designation which you held</p>
+              </div>
+            </div>
+          )}
           <div id="dv-ExperienceTeamName" className="InputWrapper">
             <input
               type="text"
@@ -87,7 +173,26 @@ const Style1 = ({ resumeData, setResumeData }) => {
             <label htmlFor="in-rb_exp_team" className="TextFieldLabel">
               Team Name
             </label>
+            <svg
+              onClick={() => showOrHideInfoDiv(`Team Name${index}`)}
+              className="InputInfoSvg"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
           </div>
+
+          {infoDiv === `Team Name${index}` && (
+            <div className="InputInfoDiv">
+              <div className="InputInfoText">
+                <p>Enter your Team's name</p>
+              </div>
+            </div>
+          )}
           <div id="dv-ExperienceRoles" className="InputWrapper">
             <input
               type="text"
@@ -100,7 +205,32 @@ const Style1 = ({ resumeData, setResumeData }) => {
             <label htmlFor="in-rb_exp_role" className="TextFieldLabel">
               Roles
             </label>
+            <svg
+              onClick={() => showOrHideInfoDiv(`Roles${index}`)}
+              className="InputInfoSvg"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
           </div>
+
+          {infoDiv === `Roles${index}` && (
+            <div className="InputInfoDiv">
+              <div className="InputInfoText">
+                <p>Enter your Roles in the job as comma separated values</p>
+              </div>
+            </div>
+          )}
+
+          {infoDiv === " " && (
+            <div className="InputInfoDiv">
+              <div className="InputInfoText"></div>
+            </div>
+          )}
         </div>
       ))}
     </>
