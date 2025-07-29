a48c67eaf73013162a72f7585f6969faac84372a
Hari <harijv0310@gmail.com>
2025-07-29 23:52:28 +0530

    Added filters to log display

--- Frontend/src/Components/Profile/SubComponents/LogMgmt/AdminLogs.jsx
+++ Frontend/src/Components/Profile/SubComponents/LogMgmt/AdminLogs.jsx
@@ -17,6 +17,12 @@ const AdminLogs = () => {
   const [logTypeRequested, setLogTypeRequested] = useState("");
   const [sortBy, setSortBy] = useState("DateNewest");
 
+  const [showFilters, setShowFilters] = useState(false);
+
+  const [linkedAccountFilter, setLinkedAccountFilter] = useState("");
+  const [createdByFilter, setCreatedByFilter] = useState("");
+  const [logFilter, setLogFilter] = useState("");
+
   useEffect(() => {
     if (!logTypeRequested) return;
 
@@ -24,6 +30,7 @@ const AdminLogs = () => {
     setLoading(true);
     setVisibleLogs([]);
     setVisibleLogsStart(0);
+    setLinkedAccountFilter(""); // Reset filter when log type changes
 
     const fetchLogs = async () => {
       try {
@@ -65,18 +72,42 @@ const AdminLogs = () => {
       }
     };
 
-    if (logTypeRequested) fetchLogs();
+    fetchLogs();
 
     return () => {
       isMounted = false;
     };
   }, [logTypeRequested]);
 
+  const applyFilterAndSort = () => {
+    let selectedLogs = logsData[sortBy] || [];
+
+    if (linkedAccountFilter.trim() !== "") {
+      selectedLogs = selectedLogs.filter((log) =>
+        log.logLinkedAccount
+          .toLowerCase()
+          .includes(linkedAccountFilter.toLowerCase())
+      );
+    }
+
+    if (createdByFilter.trim() !== "") {
+      selectedLogs = selectedLogs.filter((log) =>
+        log.logAddedBy.toLowerCase().includes(createdByFilter.toLowerCase())
+      );
+    }
+
+    if (logFilter.trim() !== "") {
+      selectedLogs = selectedLogs.filter((log) =>
+        log.logDetails.toLowerCase().includes(logFilter.toLowerCase())
+      );
+    }
+
+    setVisibleLogs(selectedLogs);
+    setTotalRecords(selectedLogs.length);
+  };
+
   useEffect(() => {
-    const selectedLogs = logsData[sortBy] || [];
-    setVisibleLogs(
-      selectedLogs.slice(visibleLogsStart, visibleLogsStart + PAGE_SIZE)
-    );
+    applyFilterAndSort();
   }, [logsData, sortBy, visibleLogsStart]);
 
   const handlePrev = () =>
@@ -87,13 +118,15 @@ const AdminLogs = () => {
       Math.min(start + PAGE_SIZE, Math.max(0, totalRecords - PAGE_SIZE))
     );
 
+  const toggleFilters = () => setShowFilters((prev) => !prev);
+
   return (
     <>
       <div className="AdminMgmtWrapper">
         <p className="AdminMgmtActionHeading">Admin Logs</p>
         <span>
-          (Choose the respective buttons to see either successful logs or error
-          logs registered for admin accounts.)
+          (Choose the respective buttons given below to see either regular logs
+          or error logs registered for admin accounts.)
         </span>
 
         <div className="AdminMgmtActions">
@@ -114,93 +147,153 @@ const AdminLogs = () => {
 
       {loading && <div>Loading logs...</div>}
 
-      {!loading && visibleLogs.length > 0 && (
-        <div className="ListAdminsWrapper">
-          <div className="AdminsListHeading">
-            <p className="AdminTableHeading">Admin Logs</p>
-            <span>
-              Total records currently:{" "}
-              <span style={{ color: "red" }}>{totalRecords}</span>
-            </span>
-          </div>
-
-          <div className="LogListSort">
-            <div className="RegisterDropDown">
-              <select
-                value={sortBy}
-                id="se-sortBy"
-                onChange={(e) => {
-                  setSortBy(e.target.value);
-                  setVisibleLogsStart(0);
-                }}
-              >
-                <option value="DateNewest">Date: Newest First</option>
-                <option value="DateOldest">Date: Oldest First</option>
-                <option value="PriorityHighest">Priority: Highest First</option>
-                <option value="PriorityLowest">Priority: Lowest First</option>
-              </select>
-              <label htmlFor="se-sortBy" className="DropDownLabel">
-                Sort By
-              </label>
+      {!loading && logTypeRequested && (
+        <>
+          <div className="LogDisplayWrapper">
+            <div className="AdminsListHeading">
+              <p className="AdminTableHeading">{logTypeRequested} Logs</p>
+              <span>
+                Total records currently:{" "}
+                <span style={{ color: "red" }}>{totalRecords}</span>
+              </span>
             </div>
+
+            <div className="LogListSort">
+              <div className="RegisterDropDown">
+                <select
+                  value={sortBy}
+                  id="se-sortBy"
+                  onChange={(e) => {
+                    setSortBy(e.target.value);
+                    setVisibleLogsStart(0);
+                  }}
+                >
+                  <option value="DateNewest">Date: Newest First</option>
+                  <option value="DateOldest">Date: Oldest First</option>
+                  <option value="PriorityHighest">
+                    Priority: Highest First
+                  </option>
+                  <option value="PriorityLowest">Priority: Lowest First</option>
+                </select>
+                <label htmlFor="se-sortBy" className="DropDownLabel">
+                  Sort By
+                </label>
+              </div>
+            </div>
+
+            <div className="LogListFilter">
+              <span onClick={toggleFilters}>
+                Click to show/hide available filters
+              </span>
+            </div>
+
+            {showFilters && (
+              <>
+                {" "}
+                <div className="LogFilterWrapper">
+                  <div className="AdminMgmtOtp">
+                    <input
+                      type="text"
+                      placeholder=" "
+                      value={linkedAccountFilter}
+                      onChange={(e) => setLinkedAccountFilter(e.target.value)}
+                    />
+                    <label className="AdminMgmtTextFieldLabel2">
+                      Linked Account
+                    </label>
+                  </div>
+                  <div className="AdminMgmtOtp">
+                    <input
+                      type="text"
+                      placeholder=" "
+                      value={createdByFilter}
+                      onChange={(e) => setCreatedByFilter(e.target.value)}
+                    />
+                    <label className="AdminMgmtTextFieldLabel2">
+                      Created By
+                    </label>
+                  </div>
+                  <div className="AdminMgmtOtp">
+                    <input
+                      type="text"
+                      placeholder=" "
+                      value={logFilter}
+                      onChange={(e) => setLogFilter(e.target.value)}
+                    />
+                    <label className="AdminMgmtTextFieldLabel2">Log</label>
+                  </div>
+                </div>
+                <button
+                  onClick={applyFilterAndSort}
+                  className="AddInputButtons"
+                >
+                  Filter
+                </button>
+              </>
+            )}
           </div>
-
-          <div className="AdminsList">
-            <table className="CurrentAdminsTable">
-              <thead>
-                <tr>
-                  <th>Linked Account</th>
-                  <th>Created By</th>
-                  <th>Created At</th>
-                  <th>Priority</th>
-                  <th>Log</th>
-                </tr>
-              </thead>
-              <tbody>
-                {visibleLogs.map((log) => (
-                  <tr key={`${log.createdAt}-${log.logDetails}`}>
-                    <td>{log.logLinkedAccount}</td>
-                    <td>{log.logAddedBy}</td>
-                    <td>{log.createdAtFormatted}</td>
-                    <td>{log.logPriority}</td>
-                    <td>{log.logDetails}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          <div className="LogsNavigation">
-            <button
-              onClick={handlePrev}
-              className="LeftNavigationButtons"
-              disabled={visibleLogsStart === 0}
-            >
-              Previous
-            </button>
-
-            <span>
-              Showing {totalRecords === 0 ? 0 : visibleLogsStart + 1} -{" "}
-              {Math.min(visibleLogsStart + PAGE_SIZE, totalRecords)} out of{" "}
-              {totalRecords} records.
+          {!loading && !visibleLogs.length && logTypeRequested && (
+            <span style={{ margin: "2rem 0rem" }}>
+              No logs found for this selection.
             </span>
+          )}
 
-            <button
-              onClick={handleNext}
-              className="LeftNavigationButtons"
-              disabled={
-                visibleLogsStart + PAGE_SIZE >= totalRecords ||
-                totalRecords === 0
-              }
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
+          {!loading && visibleLogs.length > 0 && (
+            <div className="LogDisplayWrapper">
+              <div className="AdminsList">
+                <table className="CurrentAdminsTable">
+                  <thead>
+                    <tr>
+                      <th>Linked Account</th>
+                      <th>Created By</th>
+                      <th>Created At</th>
+                      <th>Priority</th>
+                      <th>Log</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {visibleLogs.map((log) => (
+                      <tr key={`${log.createdAt}-${log.logDetails}`}>
+                        <td>{log.logLinkedAccount}</td>
+                        <td>{log.logAddedBy}</td>
+                        <td>{log.createdAtFormatted}</td>
+                        <td>{log.logPriority}</td>
+                        <td>{log.logDetails}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
 
-      {!loading && !visibleLogs.length && logTypeRequested && (
-        <span>No logs found for this selection.</span>
+              <div className="LogsNavigation">
+                <button
+                  onClick={handlePrev}
+                  className="LeftNavigationButtons"
+                  disabled={visibleLogsStart === 0}
+                >
+                  Previous
+                </button>
+
+                <span>
+                  Showing {totalRecords === 0 ? 0 : visibleLogsStart + 1} -{" "}
+                  {Math.min(visibleLogsStart + PAGE_SIZE, totalRecords)} out of{" "}
+                  {totalRecords} records.
+                </span>
+
+                <button
+                  onClick={handleNext}
+                  className="LeftNavigationButtons"
+                  disabled={
+                    visibleLogsStart + PAGE_SIZE >= totalRecords ||
+                    totalRecords === 0
+                  }
+                >
+                  Next
+                </button>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </>
   );
